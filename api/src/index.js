71a5dc87ffe1b6f63b7e198d83cb83037c4d5efa
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-06-01 22:58:27 -0300

    Add files via upload

---
+++ api/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: '172.17.0.2',
+  user: 'root',
+  password: 'lucas123',
+  database: 'programadorabordo'
+});
+connection.connect();
+
+  connection.query('SELECT * FROM products', function (error, results) {
+    
+    
+    app.get('/products', function(req, res)  {
+        res.send(results.map(item => ({ name: item.name, price: item.price })))
+      })
+      
+      app.listen(3000)
+ 
+
+    //res.send(results.map(item => ({ name: item.name, price: item.price })));
+  });
+
+
