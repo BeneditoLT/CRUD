2f992357cee37d2a8643eb60a526a8cb4c9ee80b
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-06-13 21:46:14 -0300

    Update script.sql

--- api/db/script.sql
+++ api/db/script.sql
@@ -4,7 +4,7 @@ USE programadorabordo;
 CREATE TABLE IF NOT EXISTS products (
   id INT(11) AUTO_INCREMENT,
   name VARCHAR(255),
-  price DECIMAL(10, 2),
+  price DECIMAL(12, 2),
   PRIMARY KEY (id)
 );
 
