9752ff5a85d3fc03f5220acfb3ac70b6381c5ffe
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-08-11 23:44:07 -0300

    Update script.sql

--- api/db/script.sql
+++ api/db/script.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS products (
   PRIMARY KEY (id)
 );
 
-INSERT INTO products VALUE(0, 'Curso Front-end especialista', 2500);
+INSERT INTO products VALUE(0, 'Curso Front-end especialista', 500);
 INSERT INTO products VALUE(0, 'Curso JS Fullstack', 900);
